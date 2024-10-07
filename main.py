9797a8c9186d9482b9d9c7c4c428ff1bca4474b9
megsantiu <meghansantosh@gmail.com>
2024-10-07 14:02:31 -0400

    Added main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sqlite3
 import Scorecard_DB_Manager
- 
+
 dbManager = Scorecard_DB_Manager.ScoreCardDBManager()
 data = dbManager.ReadAllData()
- 
+
 #Print Database rows
 for val in data.fetchall():
     print(val)

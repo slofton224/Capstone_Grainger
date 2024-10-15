336c59cfcf030fab9b206340f0e2e6097a7748c0
Conrad27 <tanco@iu.edu>
2024-10-15 15:50:49 -0400

    All derived columns completed.

--- Grainger_SQL.sql
+++ Grainger_SQL.sql
@@ -1,8 +1,20 @@
 -- SQLite
 
-UPDATE inbound_delivery
-SET inbound_diff_delivery = (
-  SELECT inbound_delivered_date - po_delivery_date 
-  FROM purchase_order_header 
-  WHERE purchase_order_header.po_id = inbound_delivery.po_id
+UPDATE sales_order_item
+SET so_pre_post_warranty = (
+    CASE 
+        WHEN so_warranty_calculation <= (
+            SELECT mat_warranty 
+            FROM material_master 
+            WHERE material_master.mat_id = sales_order_item.mat_id
+        ) THEN 'Pre-warranty'
+        ELSE 'Post-warranty'
+    END
+)
+WHERE so_warranty_calculation IS NOT NULL
+AND EXISTS (
+    SELECT 1 
+    FROM material_master 
+    WHERE material_master.mat_id = sales_order_item.mat_id
 );
+

23d938a4943f432f75f715ce5ace721d3da420c6
Conrad27 <tanco@iu.edu>
2024-10-21 16:04:10 -0400

    Updated main to include defect_rate

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import sqlite3
 from cost_variance_score import calculate_cost_variance_score
 from customer_review import calculate_customer_review_score
+from Defect_Rate import calculate_vendor_defect_rate
 # cost variance score against each item
 #score_df = calculate_cost_variance_score()
 #print(score_df)
 
 score_df=calculate_customer_review_score()
 print(score_df)
+
+# defect rate score against each item
+defect_score_df = calculate_vendor_defect_rate()
+print(defect_score_df)
